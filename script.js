14daef952892ac417e85c881897aaf9a19114c33
Varya Sachenko <ya.varushka@ya.ru>
2021-03-17 21:45:57 +0300

    mesto 1/2

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
-/*let formElement = document.querySelector('form');*/
-
-
-
-function say_hi() {
-    var username = document.getElementById('username').value;
-    var status = document.getElementById('status').value;
-    document.getElementById('profile_name').innerHTML = username;
-    document.getElementById('profile_status').innerHTML = status;
+function change_name () {
+    let username = document.getElementById('username').value;
+    let status = document.getElementById('status').value;
+    let name = document.getElementById('profile_name');
+    let job = document.getElementById('profile_status')
+    name.innerHTML = username;
+    job.innerHTML = status;
 }
-document.getElementById('form_save-button').addEventListener('click', say_hi);
+document.getElementById('form_save-button').addEventListener('click', change_name);
