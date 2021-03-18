9f89b8073db5f1eba1bf03d625c927a5757e48aa
Varya Sachenko <ya.varushka@ya.ru>
2021-03-18 22:59:40 +0300

    mesto 2/1

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 
-function change_name () {
-    let username = document.getElementById('username').value;
-    let status = document.getElementById('status').value;
-    let name = document.getElementById('profile_name');
-    let job = document.getElementById('profile_status')
-    name.innerHTML = username;
-    job.innerHTML = status;
+
+// Находим форму в DOM
+// Воспользуйтесь методом querySelector()
+let all = document.querySelector('.content');
+let formElement = document.querySelector('.form__fields');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.form__name')// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.form__job')// Воспользуйтесь инструментом .querySelector()
+console.log(nameInput);
+let save = formElement.querySelector('.form__save-button');
+console.log(save);
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    let username = nameInput.value;
+    console.log(username);
+    let status = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+    let name = document.querySelector('.profile__name');
+    let job = document.querySelector('.profile__status');
+    // Вставьте новые значения с помощью textContent
+    name.textContent = username;
+    job.textContent = status;
 }
-document.getElementById('form_save-button').addEventListener('click', change_name);
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 
+
+let clos = all.querySelector('.form__close-button');
+let edit = document.querySelector('.profile__edit-button-box');
+
+function popup_opened (){
+    all.classList.add('content_opened');
+}
+
+function is_close (){
+    all.classList.remove('content_opened');
+}
+
+edit.addEventListener('click', popup_opened); 
+clos.addEventListener('click', is_close); 
